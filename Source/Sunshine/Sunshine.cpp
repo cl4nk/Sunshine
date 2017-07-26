93b9ab0ee3e9ccb6c3dab69353accbc2d6fdccc0
n.fontes <n.fontes@student.isartdigital.com>
2017-07-26 18:03:48 +0200

    Init UE4 Project

---
+++ Source/Sunshine/Sunshine.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "Sunshine.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sunshine, "Sunshine" );
+ 
