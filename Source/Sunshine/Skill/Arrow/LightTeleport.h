e9dbb388cb6e4f35f34f2dc14d74f53ba494defc
Guillaume <g.newton@student.isartdigital.com>
2017-09-10 18:02:00 +0200

    Added class for LightTeleport skill

---
+++ Source/Sunshine/Skill/Arrow/LightTeleport.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Skill/SkillBase.h"
+#include "LightTeleport.generated.h"
+
+/**
+ * Teleports from a light arrow to the other light arrow
+ */
+UCLASS()
+class SUNSHINE_API ALightTeleport : public ASkillBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual ~ALightTeleport() = default;
+
+#pragma region Unreal Engine functions
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick( float deltaTime ) override;
+#pragma endregion
+
+#pragma region Events
+	virtual void OnActivationStart_Implementation() override;
+	virtual void OnActivationEnd_Implementation() override;
+#pragma endregion
+};
