1fd8fa3b1a33b58e329e83890eceb7eef260413d
Guillaume <g.newton@student.isartdigital.com>
2017-09-10 17:59:31 +0200

    Added class for HuntVision

---
+++ Source/Sunshine/Skill/HuntVision/HuntVision.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HuntVision.h"
+
+#pragma region Unreal Engine functions
+void AHuntVision::BeginPlay()
+{
+	Super::BeginPlay();
+
+	m_manaCost = 1;
+	m_noiseValue = 0.0f;
+}
+
+void AHuntVision::Tick( float deltaTime )
+{
+	Super::Tick( deltaTime );
+}
+#pragma endregion
+
+#pragma region Events
+void AHuntVision::OnActivationStart_Implementation()
+{
+}
+
+void AHuntVision::OnActivationEnd_Implementation()
+{
+}
+#pragma endregion
