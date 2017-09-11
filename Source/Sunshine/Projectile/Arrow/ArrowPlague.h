bde04373c0585550ea70bfd5ff7302943b0979ff
Guillaume <g.newton@student.isartdigital.com>
2017-09-11 18:32:37 +0200

    Push before going to UE4.17
    Need to modify Lyre Skills and Arrow classes.

---
+++ Source/Sunshine/Projectile/Arrow/ArrowPlague.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Projectile/Arrow/ArrowBase.h"
+#include "ArrowPlague.generated.h"
+
+/**
+ * Plague arrow capable of killing an enemy
+ */
+UCLASS()
+class SUNSHINE_API UArrowPlague : public UArrowBase
+{
+	GENERATED_BODY()
+};
