57c3ea4089ec266e0ba951f847a6eb8299179eb7
Guillaume <g.newton@student.isartdigital.com>
2017-09-16 23:02:29 +0200

    Implement functions and flow in HuntVision for future camera function

--- Source/Sunshine/Skill/HuntVision/HuntVision.cpp
+++ Source/Sunshine/Skill/HuntVision/HuntVision.cpp
@@ -6,14 +6,38 @@
 void AHuntVision::BeginPlay()
 {
 	Super::BeginPlay();
-
-	m_manaCost = 1;
-	m_noiseValue = 0.0f;
 }
 
 void AHuntVision::Tick( float deltaTime )
 {
 	Super::Tick( deltaTime );
+
+	switch ( m_skillState )
+	{
+		case Waiting:
+			TickWaiting();
+			break;
+		case StartHuntVision:
+			TickStartHuntVision();
+			break;
+		case ActiveHuntVision:
+			TickActiveHuntVision( deltaTime );
+			break;
+		case StopHuntVision:
+			TickStopHuntVision();
+			break;
+	}
+}
+
+void AHuntVision::Init( ASunshineCharacter* owner )
+{
+	Super::Init( owner );
+
+	// TODO: remove when used as blueprint
+	m_manaCost = 1;
+	m_noiseValue = 0.0f;
+
+	m_skillState = Waiting;
 }
 #pragma endregion
 
@@ -22,13 +46,53 @@ void AHuntVision::OnActivationStart_Implementation()
 {
 	Super::OnActivationStart_Implementation();
 
-	// Do something
+	if ( m_owner == nullptr )
+	{
+		UE_LOG( LogTemp, Error, TEXT( "Owner is nullptr !" ) );
+		return;
+	}
+
+	if ( m_skillState != Waiting )
+		return;
+
+	m_skillState = StartHuntVision;
+	SetActorTickEnabled( true );
 }
 
 void AHuntVision::OnActivationEnd_Implementation()
 {
-	// Do something
+	// Do nothing
 
 	Super::OnActivationEnd_Implementation();
+}
+
+void AHuntVision::TickWaiting()
+{
+	m_huntVisionTime = 0.0f;
+	SetActorTickEnabled( false );
+}
+
+void AHuntVision::TickStartHuntVision()
+{
+	UE_LOG( LogTemp, Warning, TEXT( "Start hunt vision !" ) );
+
+	// TODO: create function to do what the huntvision is supposed to do
+
+	m_skillState = ActiveHuntVision;
+}
+
+void AHuntVision::TickActiveHuntVision( const float deltaTime )
+{
+	m_huntVisionTime += deltaTime;
+	m_skillState = StopHuntVision;
+}
+
+void AHuntVision::TickStopHuntVision()
+{
+	// TODO: deactivate huntvision here
+
+	m_huntVisionTime = 0.0f;
+	m_skillState = Waiting;
+	UE_LOG( LogTemp, Warning, TEXT( "Stop hunt vision !" ) );
 }
 #pragma endregion
